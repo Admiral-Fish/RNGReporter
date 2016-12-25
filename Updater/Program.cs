ae7db434a002e6666d27138a437345030ea8dafd
Admiral-Fish <admiralfish420@gmail.com>
2016-12-24 18:21:27 -0700

    Initial updates

---
+++ Updater/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using Ionic.Zip;
+
+namespace Updater
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2 || !File.Exists(args[0]) ||
+                !Uri.IsWellFormedUriString(args[1], UriKind.RelativeOrAbsolute)) return;
+            var client = new WebClient();
+            string temp = Path.GetTempFileName();
+            client.DownloadFile(args[1], temp);
+            string path = Path.GetDirectoryName(args[0]);
+            using (var file = new ZipFile(temp))
+            {
+                file.ExtractExistingFile = ExtractExistingFileAction.OverwriteSilently;
+                file.ExtractAll(path);
+            }
+            File.Delete(temp);
+            Process.Start(args[0]);
+        }
+    }
+}

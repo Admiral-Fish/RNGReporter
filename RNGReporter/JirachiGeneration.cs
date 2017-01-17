439565c5bf05547615039c3ba8b4aefde390f928
Admiral-Fish <admiralfish420@gmail.com>
2017-01-17 12:55:52 -0700

    Jirachi improvment

--- RNGReporter/JirachiGeneration.cs
+++ RNGReporter/JirachiGeneration.cs
@@ -1,4 +1,4 @@
-﻿using RNGReporter.Objects;
+using RNGReporter.Objects;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -50,21 +50,8 @@ namespace RNGReporter
         private void genListOut(uint seed)
         {
             String genlistout = calcProbable(seed);
-            String genlistend = genlistout.Substring(genlistout.Length - 7);
-            int advance = 3;
-
-            if (genlistend[0] == '0')
-                advance = 7;
-            if (genlistend[2] == '0' && advance == 3)
-                advance = 5;
-            if (genlistend[4] == '0' && advance == 3)
-                advance = 3;
-            if (genlistend[6] == '0' && advance == 3)
-                advance = 3;
-            int mm = genlistout.IndexOf('M');
-            String result = genlistout.Substring(0, mm + advance) + "T" + genlistout.Substring(mm + advance);
-            result = result.Substring(2);
-            result = result.Replace("|", " | ");
+            
+            String result = genlistout.Replace("|", " | ");
             generation.Add(new ProbableGeneration
             {
                 Probable = result
@@ -79,6 +66,7 @@ namespace RNGReporter
             String genlistout = (seed >> 30).ToString();
             uint f = seed >> 30;
             uint[] checker = { 0, 0, 0, 0 };
+            uint[] compare = { 0, 1, 1, 1 };
             uint backseed = seed;
             uint advance = 8;
             bool xCheck = false;
@@ -96,24 +84,15 @@ namespace RNGReporter
                         checker[x] = 0;
                     genlistout = " M: " + genlistout;
                 }
+
                 uint g = backseed >> 30;
-                if (ms.Count == 0 && g == f)
-                {
-                    if (m >= 8)
-                    {
-                        if (!xCheck)
-                        {
-                            xCheck = true;
-                            genlistout = "XXX" + genlistout;
-                        }
-                    }
-                }
+                
                 if (g != 0)
                 {
                     if (checker[g] != 1)
                         checker[g] = 1;
                 }
-                if (checker[0] == 0 && checker[1] == 1 && checker[2] == 1 && checker[3] == 1)
+                if (checker == compare)
                 {
                     for (int x = 0; x < 4; x++)
                         checker[x] = 0;
@@ -121,7 +100,69 @@ namespace RNGReporter
                 }
                 m += 1;
             }
+
+            String genlistend = genlistout.Substring(genlistout.Length - 7);
+            int intAdvance = 3;
+
+            if (genlistend[0] == '0')
+                intAdvance = 7;
+            if (genlistend[2] == '0' && intAdvance == 3)
+                intAdvance = 5;
+            if (genlistend[4] == '0' && intAdvance == 3)
+                intAdvance = 3;
+            if (genlistend[6] == '0' && intAdvance == 3)
+                intAdvance = 3;
+            int mm = genlistout.IndexOf('M');
+            genlistout = genlistout.Substring(0, mm + intAdvance) + "T:" + genlistout.Substring(mm + intAdvance);
+            genlistout = genlistout.Substring(2);
+            genlistout = genlistout.Replace("M:", "");
+            genlistout = genlistout.Replace(" ", "");
+            int index = genlistout.IndexOf(':') + 1;
+            char targetNum = genlistout[index];
+            String string2Search = genlistout.Substring(0, index);
+            genlistend = genlistout.Substring(index);
+            string2Search = flip(string2Search);
+            for (int x = 0; x < 4; x++)
+                checker[x] = 0;
+            checker[int.Parse(targetNum.ToString())] = 1;
+            for (int x = 0; x < string2Search.Length; x++)
+            {
+                if (!xCheck)
+                {
+                    if (string2Search[x] == '1' || string2Search[x] == '2' || string2Search[x] == '3')
+                    {
+                        if (string2Search[x] == targetNum)
+                        {
+                            if (checker != compare)
+                            {
+                                string2Search = string2Search.Substring(0, x) + "XXX" + string2Search.Substring(x);
+                                xCheck = true;
+                            }
+                        }
+                        else
+                        {
+                            checker[int.Parse(string2Search[x].ToString())] = 1;
+                        }
+                    }
+                }
+            }
+
+            string2Search = flip(string2Search);
+            genlistout = string2Search + genlistend;
+
             return genlistout;
+        }
+
+        //Credits to here http://stackoverflow.com/questions/228038/best-way-to-reverse-a-string
+        public string flip(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
         }
 
         private uint reverse(uint seed)

014e3b9aec531eaab5d4a0d5523a721ac30dbc78
Admiral-Fish <admiralfish420@gmail.com>
2017-08-06 15:02:10 -0700

    Version bump

--- RNGReporter/MainForm.Designer.cs
+++ RNGReporter/MainForm.Designer.cs
@@ -2115,7 +2115,7 @@ namespace RNGReporter
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = " RNG Reporter 10.2.0";
+            this.Text = " RNG Reporter 10.2.1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.contextMenuStripGrid.ResumeLayout(false);
